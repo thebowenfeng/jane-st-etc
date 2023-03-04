5e7938cb50afc725aee401f204652e5ef9f63613
ChangXu <82389819+changxu1130@users.noreply.github.com>
2023-03-04 15:42:02 +1100

    test fair value function 05 initialise fair_values-appro initialize to None

--- sample-bot-02.py
+++ sample-bot-02.py
@@ -38,7 +38,7 @@ def main():
     buy_data = {"GS": [], "VALBZ": [], "VALE": [], "GS": [], "MS": [], "WFC": [], "XLF": []}
 
     # access fair_value if exist by fair_values_appro["stock_name"]
-    fair_values_appro = {"BOND": [], "GS": [], "MS": [], "WFC": [], "ETF": []}
+    fair_values_appro = {"BOND": None, "GS": None, "MS": None, "WFC": None, "ETF": None}
 
     # Store and print the "hello" message received from the exchange. This
     # contains useful information about your positions. Normally you start with
